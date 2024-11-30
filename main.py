73717fb2de73ff3ec07764424c256894f46855dd
LUNEi3 <151994903+LUNEi3@users.noreply.github.com>
2024-11-30 19:28:09 +0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import encrypt as lune
-
-password = input("Enter password: ")
-ep = lune.encrypt(password)
-dp = lune.decrypt(ep)
-
-print(f"plain : {password}")
-print(f"plain -> encrypt : {ep}")
-print(f"encrypt -> decrypt : {dp}")
